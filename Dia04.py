782771ce866686bde7f706ce7d57f0068b4b0e20
Welliton da Silva Gomes <137785960+WellitonGomes@users.noreply.github.com>
2024-04-19 11:13:58 -0300

    Add files via upload

---
+++ Dia04.py
@@ -0,0 +1,23 @@
+numero = int(input("Digite um numero inteiro"))
+#entra com um numero inteiro para a
+#variavel a
+#b = input("Digitei um numero inteiro 'B' ")
+#entra com um numero inteiro para b
+#variavel b
+if numero > 0:
+#se a for maior que b então imprime a
+#linha seguinte
+    print("O numero e positivo")
+#mostra a mensagem que a é maior que b
+elif numero < 0:
+#se B for maior que A mostra a seguinte mensagem
+    print("O numero é negativo")
+#imprime a mensagem que B é igual á A
+#elif b == a:
+#se B for igual a A mostra a seguinte mensagem
+#    print("B é igual a A")
+#mostra a mensagem que B é igual a A
+else:
+#se nada do que foi setado acima ocorre mostra a seguinte mensagem
+    print("O numero 0 é 0 ")
+#mostra a mensagem que ocorreu algum erro
